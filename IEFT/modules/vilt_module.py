5a70c3135aef4e2840a95b5a80a2441189203128
TangXu <74549002+TangXu-Group@users.noreply.github.com>
2023-05-30 16:09:33 +0800

    Add files via upload

---
+++ IEFT/modules/vilt_module.py
@@ -0,0 +1,294 @@
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
+import IEFT.modules.vision_transformer as vit
+from .utils import localTransformer
+from transformers.models.bert.modeling_bert import BertConfig, BertEmbeddings
+from IEFT.modules import heads, objectives, vilt_utils
+import time
+
+class ViLTransformerSS(pl.LightningModule):
+    def __init__(self, config):
+        super().__init__()
+        self.save_hyperparameters()
+
+        bert_config = BertConfig(
+            vocab_size=config["vocab_size"],
+            hidden_size=config["hidden_size"],
+            num_hidden_layers=config["num_layers"],
+            num_attention_heads=config["num_heads"],
+            intermediate_size=config["hidden_size"] * config["mlp_ratio"],
+            max_position_embeddings=config["max_text_len"],
+            hidden_dropout_prob=config["drop_rate"],
+            attention_probs_dropout_prob=config["drop_rate"],
+        )
+
+        self.text_embeddings = BertEmbeddings(bert_config)
+        self.text_embeddings.apply(objectives.init_weights)
+
+        self.token_type_embeddings = nn.Embedding(2, config["hidden_size"])
+        self.token_type_embeddings.apply(objectives.init_weights)
+
+        ffn_expansion_factor = 2.66
+        LayerNorm_type = 'WithBias'
+
+        self.local_net = nn.ModuleList(
+            [localTransformer(dim=768,
+                              num_heads=8,
+                              ffn_expansion_factor=ffn_expansion_factor,
+                              bias=False,
+                              LayerNorm_type=LayerNorm_type)
+             for i in range(3)])
+
+        if self.hparams.config["load_path"] == "":
+            self.transformer = getattr(vit, self.hparams.config["vit"])(
+                pretrained=True, config=self.hparams.config
+            )
+        else:
+            self.transformer = getattr(vit, self.hparams.config["vit"])(
+                pretrained=False, config=self.hparams.config
+            )
+
+        self.pooler = heads.Pooler(config["hidden_size"])
+        self.pooler.apply(objectives.init_weights)
+
+        if config["loss_names"]["mlm"] > 0:
+            self.mlm_score = heads.MLMHead(bert_config)
+            self.mlm_score.apply(objectives.init_weights)
+
+        if config["loss_names"]["itm"] > 0:
+            self.itm_score = heads.ITMHead(config["hidden_size"])
+            self.itm_score.apply(objectives.init_weights)
+
+        if config["loss_names"]["mpp"] > 0:
+            self.mpp_score = heads.MPPHead(bert_config)
+            self.mpp_score.apply(objectives.init_weights)
+
+        # ===================== Downstream ===================== #
+        if (
+            self.hparams.config["load_path"] != ""
+            and not self.hparams.config["test_only"]
+        ):
+            ckpt = torch.load(self.hparams.config["load_path"], map_location="cpu")
+            state_dict = ckpt["state_dict"]
+            self.load_state_dict(state_dict, strict=False)
+
+        hs = self.hparams.config["hidden_size"]
+
+        if self.hparams.config["loss_names"]["vqa"] > 0:
+            vs = self.hparams.config["vqav2_label_size"]
+            self.vqa_classifier = nn.Sequential(
+                nn.Linear(hs, hs * 2),
+                nn.LayerNorm(hs * 2),
+                nn.GELU(),
+                nn.Linear(hs * 2, vs),
+            )
+            self.vqa_classifier.apply(objectives.init_weights)
+
+        if self.hparams.config["loss_names"]["nlvr2"] > 0:
+            self.nlvr2_classifier = nn.Sequential(
+                nn.Linear(hs * 2, hs * 2),
+                nn.LayerNorm(hs * 2),
+                nn.GELU(),
+                nn.Linear(hs * 2, 2),
+            )
+            self.nlvr2_classifier.apply(objectives.init_weights)
+            emb_data = self.token_type_embeddings.weight.data
+            self.token_type_embeddings = nn.Embedding(3, hs)
+            self.token_type_embeddings.apply(objectives.init_weights)
+            self.token_type_embeddings.weight.data[0, :] = emb_data[0, :]
+            self.token_type_embeddings.weight.data[1, :] = emb_data[1, :]
+            self.token_type_embeddings.weight.data[2, :] = emb_data[1, :]
+
+        if self.hparams.config["loss_names"]["irtr"] > 0:
+            self.rank_output = nn.Linear(hs, 1)
+            self.rank_output.weight.data = self.itm_score.fc.weight.data[1:, :]
+            self.rank_output.bias.data = self.itm_score.fc.bias.data[1:]
+            self.margin = 0.2
+            for p in self.itm_score.parameters():
+                p.requires_grad = False
+
+        vilt_utils.set_metrics(self)
+        self.current_tasks = list()
+
+        # ===================== load downstream (test_only) ======================
+
+        if self.hparams.config["load_path"] != "" and self.hparams.config["test_only"]:
+            ckpt = torch.load(self.hparams.config["load_path"], map_location="cpu")
+            state_dict = ckpt["state_dict"]
+            self.load_state_dict(state_dict, strict=False)
+
+    def infer(
+        self,
+        batch,
+        mask_text=False,
+        mask_image=False,
+        image_token_type_idx=1,
+        image_embeds=None,
+        image_masks=None,
+    ):
+        if f"image_{image_token_type_idx - 1}" in batch:
+            imgkey = f"image_{image_token_type_idx - 1}"
+        else:
+            imgkey = "image"
+        # print("this is the batch of img:{}".format(batch.keys()))
+        do_mlm = "_mlm" if mask_text else ""
+        text_ids = batch[f"text_ids{do_mlm}"]
+        text_labels = batch[f"text_labels{do_mlm}"]
+        text_masks = batch[f"text_masks"]
+        text_embeds = self.text_embeddings(text_ids)
+
+        if image_embeds is None and image_masks is None:
+            img = batch[imgkey][0]
+            (
+                image_embeds,
+                image_masks,
+                patch_index,
+                image_labels,
+            ) = self.transformer.visual_embed(
+                img,
+                max_image_len=self.hparams.config["max_image_len"],
+                mask_it=mask_image,
+            )
+        else:
+            patch_index, image_labels = (
+                None,
+                None,
+            )
+
+        text_embeds, image_embeds = (
+            text_embeds + self.token_type_embeddings(torch.zeros_like(text_masks)),
+            image_embeds
+            + self.token_type_embeddings(
+                torch.full_like(image_masks, image_token_type_idx)
+            ),
+        )
+
+        co_embeds = torch.cat([text_embeds, image_embeds], dim=1)
+        co_masks = torch.cat([text_masks, image_masks], dim=1)
+
+        x = co_embeds
+
+        for i in range(len(self.transformer.blocks)):
+            blk = self.transformer.blocks[i]
+            text_feats_block, image_feats_block = (
+                x[:, : text_embeds.shape[1]],
+                x[:, text_embeds.shape[1]:],
+            )
+            if i == 1 or i == 2 or i==3:
+                block_id = i-1
+
+                blk_se = self.transformer.SE_blocks[block_id]
+
+                img_cls, image_feats_block = image_feats_block[:, 0], image_feats_block[:, 1:]
+                img_cls = img_cls.unsqueeze(dim=1)
+
+
+                image_feats_block_se = image_feats_block.permute(0, 2, 1)
+                image_feats_block, _attn = blk_se(image_feats_block_se)
+                image_feats_block = image_feats_block.permute(0, 2, 1)
+
+                image_feats_block = self.local_net[block_id](image_feats_block)
+
+                image_feats_block = torch.cat([img_cls, image_feats_block], dim=1)
+                x = torch.cat([text_feats_block, image_feats_block], dim=1)
+
+            if i!=11 and i!=10 and i!=9 and i!=8 and i!=7 and i!=6 :
+
+                x, _attn = blk(x, mask=co_masks)
+
+            else:
+                x = x
+        out_feats = x
+        x = self.transformer.norm(x)
+        text_feats, image_feats = (
+            x[:, : text_embeds.shape[1]],
+            x[:, text_embeds.shape[1] :],
+        )
+        cls_feats = self.pooler(x)
+        ret = {
+            "text_feats": text_feats,
+            "image_feats": image_feats,
+            "cls_feats": cls_feats,
+            "raw_cls_feats": x[:, 0],
+            "image_labels": image_labels,
+            "image_masks": image_masks,
+            "text_labels": text_labels,
+            "text_ids": text_ids,
+            "text_masks": text_masks,
+            "patch_index": patch_index,
+            "out_feats":out_feats
+        }
+
+        return ret
+
+    def forward(self, batch):
+        ret = dict()
+        if len(self.current_tasks) == 0:
+            ret.update(self.infer(batch))
+            return ret
+
+        # Masked Language Modeling
+        if "mlm" in self.current_tasks:
+            ret.update(objectives.compute_mlm(self, batch))
+
+        # Masked Patch Prediction
+        if "mpp" in self.current_tasks:
+            ret.update(objectives.compute_mpp(self, batch))
+
+        # Image Text Matching
+        if "itm" in self.current_tasks:
+            ret.update(objectives.compute_itm_wpa(self, batch))
+
+        # Visual Question Answering
+        if "vqa" in self.current_tasks:
+            ret.update(objectives.compute_vqa(self, batch))
+
+        # Natural Language for Visual Reasoning 2
+        if "nlvr2" in self.current_tasks:
+            ret.update(objectives.compute_nlvr2(self, batch))
+
+        # Image Retrieval and Text Retrieval
+        if "irtr" in self.current_tasks:
+            ret.update(objectives.compute_irtr(self, batch))
+
+        return ret
+
+    def training_step(self, batch, batch_idx):
+        vilt_utils.set_task(self)
+        output = self(batch)
+        total_loss = sum([v for k, v in output.items() if "loss" in k])
+
+        return total_loss
+
+    def training_epoch_end(self, outs):
+        vilt_utils.epoch_wrapup(self)
+
+    def validation_step(self, batch, batch_idx):
+        vilt_utils.set_task(self)
+        output = self(batch)
+
+    def validation_epoch_end(self, outs):
+        vilt_utils.epoch_wrapup(self)
+
+    def test_step(self, batch, batch_idx):
+        vilt_utils.set_task(self)
+        output = self(batch)
+        # print(output)
+
+        ret = dict()
+
+        if self.hparams.config["loss_names"]["vqa"] > 0:
+            ret.update(objectives.vqa_test_step(self, batch, output))
+
+        return ret
+
+    def test_epoch_end(self, outs):
+        model_name = self.hparams.config["load_path"].split("/")[-1][:-5]
+        # print(outs)
+        if self.hparams.config["loss_names"]["vqa"] > 0:
+            objectives.vqa_test_wrapup(outs, model_name)
+        vilt_utils.epoch_wrapup(self)
+
+    def configure_optimizers(self):
+        return vilt_utils.set_schedule(self)
